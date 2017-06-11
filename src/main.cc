a127da00d435ca663c82a6295a6e360cf8202a4b
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2017-06-11 18:28:05 +1000

    Include gflags library

--- src/main.cc
+++ src/main.cc
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <vector>
 
+#include <gflags/gflags.h>
+
 #include "huffman.h"
 
 using namespace std;
 
+DEFINE_bool(verbose, false, "Show mapping from char to bits for encoder/decoder");
+
 int main(int argc, char *argv[]) {
     auto encoder = HuffmanEncoder();
+
+    gflags::SetUsageMessage("Huffman usage:");
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
 
     string input = "cccbbdddda";
     vector<char> buf = vector<char>(input.begin(), input.end());
